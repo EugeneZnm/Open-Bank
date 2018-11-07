b9409e755740b54e861ba2fea71d090a08b8d648
EugeneZnm <eugenenzioki@gmail.com>
2018-11-07 12:19:49 +0300

    addition of uer siginup and login forms

---
+++ webank/forms.py
@@ -0,0 +1,60 @@
+import datetime
+from django import forms
+from django.contrib.auth import authenticate
+from django.contrib.auth import UserCreationForm
+from django.forms.extras.widgets import SelectDateWidget
+
+# import user model
+from .models import User
+
+class UserRegistrationForm(UserCreationForm):
+    born = forms.DateField(
+        widget=SelectDateWidget()
+    )
+
+    class Meta:
+        model = User
+        fields = ["full_name",
+                  "gender",
+                  "born",
+                  "email,"
+                  "phone",
+                  "city",
+                  "nationality",
+                  "account_type",
+                  "picture",
+                  "password1",
+                  "password2"
+                  ]
+
+    def save(self, commit = True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['full_name']
+        user.born = self.cleaned_data['birth_date']
+        if commit:
+            user.save()
+        return user
+
+
+class UserLoginForm(forms.Form):
+    """
+    form for allowing user login
+    """
+    def clean(self, *args, *kwargs):
+        account_no = self.cleaned_data.get("account_no")
+        password = self.cleaned_data.get("password")
+
+        if account_no and password:
+            user_obj = User.objects.filter(account_no=account_no.first)
+            if user_obj:
+                user = authenticate(email=user_obj.email, password=password)
+                if not user:
+                    raise forms.ValidationError("This Account Does Not Exist, Check if your Input is Correct")
+                if not user.check_password(password):
+                    raise forms.ValidationError("Password Does Not Match")
+                if not user.is_active:
+                    raise forms.ValidationError("Account is Inactive")
+            else:
+                raise forms.ValidationError("This Account Doesn't Exist.")
+        return super(UserLoginForm, self).clean( *args, **kwargs)
